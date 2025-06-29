ebbf17645676c428d63ce7f373f99f5a1ab5342d
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-06-29 11:28:49 +0200

    1.19.2

--- src/main/java/com/leclowndu93150/lavachickenmusicdisc/LavaChicken.java
+++ src/main/java/com/leclowndu93150/lavachickenmusicdisc/LavaChicken.java
@@ -1,23 +1,12 @@
 package com.leclowndu93150.lavachickenmusicdisc;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Zombie;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.RecordItem;
-import net.minecraft.world.item.Rarity;
-import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraft.world.item.*;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.common.data.LanguageProvider;
-import net.minecraftforge.data.event.GatherDataEvent;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -38,11 +27,11 @@ public class LavaChicken {
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MODID);
 
     public static final RegistryObject<SoundEvent> MUSIC_DISC_LAVA_CHICKEN_SOUND = SOUNDS.register("music_disc.lava_chicken",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "music_disc.lava_chicken")));
+            () -> new SoundEvent(new ResourceLocation(MODID, "music_disc.lava_chicken")));
 
     public static final RegistryObject<Item> MUSIC_DISC_LAVA_CHICKEN = ITEMS.register("music_disc_lava_chicken",
             () -> new RecordItem(15, MUSIC_DISC_LAVA_CHICKEN_SOUND.get(),
-                    new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 3080));
+                    new Item.Properties().stacksTo(1).rarity(Rarity.RARE).tab(CreativeModeTab.TAB_TOOLS), 3080));
 
     public LavaChicken() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -50,15 +39,7 @@ public class LavaChicken {
         ITEMS.register(modEventBus);
         SOUNDS.register(modEventBus);
 
-        modEventBus.addListener(this::addCreative);
-
         MinecraftForge.EVENT_BUS.register(new ForgeEvents());
-    }
-
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
-            event.accept(MUSIC_DISC_LAVA_CHICKEN);
-        }
     }
 
     public static class ForgeEvents {
@@ -67,7 +48,7 @@ public class LavaChicken {
             if (event.getEntity() instanceof Zombie zombie && zombie.isBaby() &&
                     zombie.getVehicle() != null && zombie.getVehicle().getType() == EntityType.CHICKEN) {
                 event.getDrops().add(new net.minecraft.world.entity.item.ItemEntity(
-                        event.getEntity().level(),
+                        event.getEntity().level,
                         event.getEntity().getX(),
                         event.getEntity().getY(),
                         event.getEntity().getZ(),
